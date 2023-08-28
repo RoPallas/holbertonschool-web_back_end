704376efdc12bba2f0d3b87e0428648d9f01923c
RoPallas <rocio.orquestas@gmail.com>
2023-08-28 08:07:35 -0700

    Fix style problems in 3 and 9

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -4,7 +4,7 @@ type-annotated function to_str
 """
 
 
-def to_str(n:float) -> str:
+def to_str(n: float) -> str:
     """
     takes a float n as argument and returns the
     string representation of the float.
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -6,4 +6,8 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Return a list of tuples with an element and
+    the element's length
+    """
     return [(i, len(i)) for i in lst]
