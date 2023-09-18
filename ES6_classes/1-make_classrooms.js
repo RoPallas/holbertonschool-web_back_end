e200804bad910caba059bc29ae097689767c2304
RoPallas <rocio.orquestas@gmail.com>
2023-09-18 11:02:36 -0700

    All tasks

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from "./0-classroom";
+
+function initializeRooms() {
+    const salon1 = new ClassRoom(19);
+    const salon2 = new ClassRoom(20);
+    const salon3 = new ClassRoom(34);
+    const listaSalones = [salon1, salon2, salon3];
+    return listaSalones;
+};
+
+module.exports = initializeRooms;
